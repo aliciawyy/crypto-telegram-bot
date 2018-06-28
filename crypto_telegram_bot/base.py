65f7584d8fea795aec572f35059adac535f3f7ef
alice <rainingilove@gmail.com>
2018-06-28 11:47:12 +0200

    add restricted id

--- crypto_telegram_bot/base.py
+++ crypto_telegram_bot/base.py
@@ -1,8 +1,45 @@
-from functools import lru_cache
+import os
+from functools import lru_cache, wraps
 
 from telegram.ext import Updater
+
+TOKEN = os.environ["TL_CRYPTO_BOT"]
+_USERS_FILENAME = os.path.join(
+    os.path.dirname(__file__), "..", "data", "users.txt"
+)
+with open(_USERS_FILENAME, "r") as f:
+    ALL_USERS = set(f.readlines())
+SECRET_CODE = "3561"
 
 
 @lru_cache(maxsize=10)
 def updater(token):
     return Updater(token=token)
+
+
+def restricted(func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in ALL_USERS:
+            bot.send_message(
+                chat_id=update.message.chat_id,
+                text="Sorry, you need to /register first with a code."
+            )
+            return
+        return func(bot, update, *args, **kwargs)
+    return wrapped
+
+
+def register(bot, update, args):
+    if len(args) == 1 and args[0] == SECRET_CODE:
+        user_id = update.effective_user.id
+        ALL_USERS.add(user_id)
+        with open(_USERS_FILENAME, "a") as f_:
+            f_.write("\n{}".format(user_id))
+        text = "Registration successful! :)"
+    else:
+        text = "Sorry, the registration code is wrong :("
+    bot.send_message(
+        chat_id=update.message.chat_id, text=text
+    )
--- crypto_telegram_bot/tuto.py
+++ crypto_telegram_bot/tuto.py
@@ -1,11 +1,14 @@
-import os
+
 import logging
 import time
 import telegram
 from telegram.ext import (
-    CommandHandler, MessageHandler, Filters, InlineQueryHandler
+    CommandHandler, MessageHandler, Filters, InlineQueryHandler,
 )
-from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram import (
+    InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
 
 
 from . import base
@@ -14,17 +17,15 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-TOKEN = os.environ["TL_CRYPTO_BOT"]
-
 
 def print_bot_information():
-    bot_ = telegram.Bot(token=TOKEN)
+    bot_ = telegram.Bot(token=base.TOKEN)
     print(bot_.get_me())
 
 
 print_bot_information()
 
-updater = base.Updater(token=TOKEN)
+updater = base.Updater(token=base.TOKEN)
 dispatcher = updater.dispatcher
 
 
@@ -42,6 +43,7 @@ def echo(bot, update):
     )
 
 
+@base.restricted
 def caps(bot, update, args):
     chat_id = update.message.chat_id
     if len(args) >= 2:
@@ -99,10 +101,44 @@ def inline_caps(bot, update):
     bot.answer_inline_query(update.inline_query.id, result)
 
 
+def build_menu(buttons,
+               n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    return menu
+
+
+def _print(bot, update, chat_data):
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Would you mind {}".format(chat_data)
+    )
+
+
+def buttons_show(bot, update):
+    button_list = [
+        InlineKeyboardButton("col1", callback_data=_print),
+        InlineKeyboardButton("col2", callback_data=_print),
+        InlineKeyboardButton("row 2", callback_data=_print)
+    ]
+    reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=2))
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="A two-column menu", reply_markup=reply_markup
+    )
+
+
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('register', base.register, pass_args=True))
 dispatcher.add_handler(CommandHandler('caps', caps, pass_args=True))
 dispatcher.add_handler(CommandHandler('nice_caps', nice_caps, pass_args=True))
 dispatcher.add_handler(CommandHandler('request', request_location))
+dispatcher.add_handler(CommandHandler('buttons', buttons_show))
 dispatcher.add_handler(InlineQueryHandler(inline_caps))
 
 # echo all text messages
